895fd6cb6a5a51fc4572f2aa1ac89fd2b3656e8c
Darjan Utmar <darjan.utmar@gmail.com>
2024-02-14 18:16:48 +0100

    Resolved problem with logout api

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -58,7 +58,9 @@ class AuthController extends Controller
     }
 
     public function logout(Request $request) {
-        auth()->user()->tokens()->delete();
+        //$request->user()->currentAccessToken()->delete();
+        //auth()->user()->tokens()->delete();
+        $request->user()->tokens()->delete();
 
         return [
             'message' => 'Logged out'
