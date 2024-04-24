08c032d3bfe3512ab50c622bb534f15d76aedc87
Darjan Utmar <darjan.utmar@gmail.com>
2024-04-24 13:37:26 +0200

    Stripe API

--- app/Http/Controllers/StripePaymentController.php
+++ app/Http/Controllers/StripePaymentController.php
@@ -10,7 +10,19 @@ class StripePaymentController extends Controller
 {
     public function stripePost(Request $request) {
         try {
-            
+            //$stripe = new \Stripe\StripeClient('sk_test_51P6x9BICSrlzlIyRF5PSlHZBHD3XQRmcTp8VqkqSoCFi9GkpC65xzmq2yoqPZ3TDhrCmnOPneGQg9JsxNeJJkoBO00wnDAlS2F');
+            $stripe = new \Stripe\StripeClient(
+                env('STRIPE_SECRET')
+            );
+
+            $res = $stripe->tokens->create([
+                'card' => [
+                    'number' => $request->number,
+                    'exp_month' =>  $request->exp_number,
+                    'exp_year' => $request->exp_year,
+                    'cvc' => $request->cvc,
+                ],
+            ]);
         } catch(Exception $ex) {
             return response() -> json([['response' => 'Error']], 500);
         }
