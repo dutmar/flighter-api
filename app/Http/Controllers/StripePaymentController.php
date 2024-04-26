64db85211405a76381346ea6045692c60e35a0d7
Darjan Utmar <darjan.utmar@gmail.com>
2024-04-26 10:12:53 +0200

    Stripe working

--- app/Http/Controllers/StripePaymentController.php
+++ app/Http/Controllers/StripePaymentController.php
@@ -18,29 +18,9 @@ class StripePaymentController extends Controller
             $response = $stripe->paymentIntents->create([
                 'amount' => $request->amount,
                 'currency' => 'eur',
-                'automatic_payment_methods' => ['enabled' => true],
-                'payment_method' => 'pm_card_visa',
             ]);
 
-            // $res = $stripe->tokens->create([
-            //     'card' => [
-            //         'number' => $request->number,
-            //         'exp_month' =>  $request->exp_month,
-            //         'exp_year' => $request->exp_year,
-            //         'cvc' => $request->cvc,
-            //     ],
-            // ]);
-
-            // Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
-
-            // $response = $stripe->charges->create([
-            //     'amount' => $request->amount,
-            //     'currency' => 'eur',
-            //     'source' => $res->id,
-            //     'token' => 'tok_visa',
-            // ]);
-
-            return response() -> json([$response], 201);
+            return response() -> json($response->client_secret, 201);
             //return response()->json(['client_secret' => $response->client_secret]);
         } catch(Exception $ex) {
             return response() -> json([['response' => 'Error']], 500);
